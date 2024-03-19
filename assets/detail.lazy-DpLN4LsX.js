62e55a75ac8526e4bf40e2dd1b6e81def53eebee
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-03-19 20:47:04 +0100

    Updates

--- assets/detail.lazy-DpLN4LsX.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e}from"./index-CYuMF4pL.js";import{T as o}from"./index-C7CvGdqh.js";const n=t("/detail")({component:r});function r(){return e.jsx(o,{children:"Hello World!"})}export{n as Route};
