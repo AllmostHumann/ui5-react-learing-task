809e01602416e14951a725d84ffb94b88fa3607d
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-03-19 20:07:33 +0100

    Updates

--- assets/detail.lazy-wZLyDMU0.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e}from"./index-DttttElj.js";import{T as o}from"./index-C4Ktr2JN.js";const n=t("/detail")({component:r});function r(){return e.jsx(o,{children:"Hello World!"})}export{n as Route};
