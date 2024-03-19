55474760caec92c18a6f05c1c26fdfa08cf94557
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-03-19 21:25:16 +0100

    Updates

---
+++ assets/about.lazy-DwiUB4UU.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-BmDiCyq8.js";const a=o("/about")({component:()=>t.jsx("div",{children:"Hello /about!"})});export{a as Route};
---
+++ assets/detail.lazy-C9hF1M5B.js
@@ -0,0 +1,1 @@
+import{c as t,j as e}from"./index-BmDiCyq8.js";import{T as o}from"./index-NBI14TND.js";const n=t("/detail")({component:r});function r(){return e.jsx(o,{children:"Hello World!"})}export{n as Route};
