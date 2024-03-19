2c246e87b4b03240b2dad14448559726a4c0b0ea
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-03-19 21:52:41 +0100

    node types installed

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,20 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { ShellBarComponent } from '../Components/ShellBarComponent/ShellBarComponent';
+import { Suspense } from 'react';
+import React from 'react';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,7 +22,9 @@ export const Route = createRootRoute({
       <ShellBarComponent />
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
 });
