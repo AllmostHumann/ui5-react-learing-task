11c11d3e0f4ff3749484cd564c3aabf8b1422024
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2024-03-19 21:53:06 +0100

    Updates

--- assets/detail.lazy-C11hyHkb.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e}from"./index-AUoIQRfo.js";import{T as o}from"./index-BRjIHWkj.js";const n=t("/detail")({component:r});function r(){return e.jsx(o,{children:"Hello World!"})}export{n as Route};
